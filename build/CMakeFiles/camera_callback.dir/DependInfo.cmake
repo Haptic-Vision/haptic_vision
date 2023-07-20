81506a24343a9f97079b35fef03cb736ff105534
Joseph Joel <josephjoel3099@gmail.com>
2023-07-20 23:05:13 +0100

    cmake updated with project name

---
+++ build/CMakeFiles/camera_callback.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/joseph/haptic_vision_joel_ss/lib/camera/CameraCapture.cpp" "/home/joseph/haptic_vision_joel_ss/build/CMakeFiles/camera_callback.dir/lib/camera/CameraCapture.cpp.o"
+  "/home/joseph/haptic_vision_joel_ss/src/main.cpp" "/home/joseph/haptic_vision_joel_ss/build/CMakeFiles/camera_callback.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/camera"
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/camera_callback.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/camera_callback.dir/src/main.cpp.o"
+  "CMakeFiles/camera_callback.dir/lib/camera/CameraCapture.cpp.o"
+  "camera_callback.pdb"
+  "camera_callback"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/camera_callback.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
